2a731837c9b57eb0f15bb9c2091f68728502ed20
melih <melihcagan.ari@kafein.com.tr>
2025-08-05 16:39:58 +0300

    trying the tag issue on actions

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -33,7 +33,7 @@ export default function Map() {
       // Load roads
       const marmarisRoads = await fetch('/data/roads.json').then(res => res.json());
       const cesmeRoads = await fetch('/data/cesme_roads.json').then(res => res.json());
-
+      // deneme
       // Merge roads
       const combinedRoads = {
         ...marmarisRoads,
